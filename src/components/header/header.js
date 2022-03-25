9fc420177254255530299d4aa5818ac713c3297f
mizabun_ <mizabun010802@gmail.com>
2022-03-25 09:34:28 +0700

    add page home

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='mx-24'>
+            <h1 id='header' className='mx-auto my-6 font-black text-5xl'>Kafa</h1>
+        </div>
+    )
+}
