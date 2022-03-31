5642018fe45899cf198b7d3dd387a24d8ebb9e17
mizabun_ <mizabun010802@gmail.com>
2022-03-31 21:37:58 +0700

    alt prop

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -11,7 +11,7 @@ export default function Home() {
         <>
             <div className='relative min-h-screen'>
                 <img src={Ellipse1} alt='' className='absolute top-0 left-0 -z-40' />
-                <img src={Rectangle1} atl='' className='absolute top-44 left-96 -z-40' />
+                <img src={Rectangle1} alt='' className='absolute top-44 left-96 -z-40' />
                 <img src={Dot} alt='' className='absolute mt-96 right-64 -z-40' />
                 <div className='bg-violet-50 h-screen w-screen absolute -z-50' />
                 <Header />
