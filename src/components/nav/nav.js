079ee3cfdca2f01f4965740eb32766f725cf0acf
mizabun_ <mizabun010802@gmail.com>
2022-04-22 02:57:29 +0700

    transisi navbar

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -8,29 +8,31 @@ export default function Navbar() {
     return (
         <>
             <nav>
-                {
-                    show ?
-                        <div className='block left-2/4 -translate-x-2/4 fixed bottom-4'>
-                            <div className='bg-indigo-900 bg-opacity-30 rounded-full p-2 space-x-5 flex items-center'>
-                                <a href='#header' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
-                                    <img src={home} alt='' className='mix-blend-color-burn' />
-                                </a>
-                                <a href='#layanan' onClick={() => setActiveNav('#layanan')} className={activeNav === '#layanan' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
-                                    <img src={layanan} alt='' className='mix-blend-color-burn' />
-                                </a>
-                                <a href='#harga' onClick={() => setActiveNav('#harga')} className={activeNav === '#harga' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
-                                    <img src={layanan} alt='' className='mix-blend-color-burn' />
-                                </a>
-                                <a href='#footer' onClick={() => setActiveNav('#footer')} className={activeNav === '#footer' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
-                                    <img src={home} alt='' className='mix-blend-color-burn' />
-                                </a>
+                <div className={`${show ? '' : 'transform translate-y-14'} block left-2/4 -translate-x-2/4 fixed bottom-4 transition-transform duration-1000`}>
+                    <div className=' rounded-full bg-indigo-900 bg-opacity-30 '>
+                        <div className='flex justify-center' onClick={() => setShow(!show)}>
+                            <div className={`${show ? '' : 'rotate-180 translate-y-2/3 '} flex justify-center pt-2 transition duration-1000`}>
+                                <div className='h-1 w-4 rounded-md bg-slate-700 rotate-45'></div>
+                                <div className='h-1 w-4 rounded-md bg-slate-700 -rotate-45 -ml-2'></div>
                             </div>
                         </div>
-                        : null
-                }
+                        <div className='flex items-center space-x-5 p-2'>
+                            <a href='#header' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
+                                <img src={home} alt='' className='mix-blend-color-burn' />
+                            </a>
+                            <a href='#layanan' onClick={() => setActiveNav('#layanan')} className={activeNav === '#layanan' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
+                                <img src={layanan} alt='' className='mix-blend-color-burn' />
+                            </a>
+                            <a href='#harga' onClick={() => setActiveNav('#harga')} className={activeNav === '#harga' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
+                                <img src={layanan} alt='' className='mix-blend-color-burn' />
+                            </a>
+                            <a href='#footer' onClick={() => setActiveNav('#footer')} className={activeNav === '#footer' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
+                                <img src={home} alt='' className='mix-blend-color-burn' />
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </nav >
-            <button onClick={() => setShow(false)} className={show === false ? 'hidden' : 'block right-10 rotate-90  fixed bottom-28'}>Hide Navbar</button>
-            <button onClick={() => setShow(true)} className={show === true ? 'hidden' : 'block right-9 rotate-90 fixed bottom-28'}>Show Navbar</button>
         </>
     )
 }
