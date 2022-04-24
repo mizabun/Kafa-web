a932744affb68623a319a4d1bf4fe25e49cb3bca
mizabun_ <mizabun010802@gmail.com>
2022-04-24 09:38:22 +0700

    navbar transisi 2

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -9,13 +9,13 @@ export default function Navbar() {
         <>
             <nav>
                 <div className={`${show ? '' : 'transform translate-y-14'} block left-2/4 -translate-x-2/4 fixed bottom-4 transition-transform duration-1000`}>
-                    <div className=' rounded-full bg-indigo-900 bg-opacity-30 '>
-                        <div className='flex justify-center' onClick={() => setShow(!show)}>
-                            <div className={`${show ? '' : 'rotate-180 translate-y-2/3 '} flex justify-center pt-2 cursor-pointer transition duration-1000`}>
-                                <div className='h-1 w-4 rounded-md bg-slate-700 rotate-45'></div>
-                                <div className='h-1 w-4 rounded-md bg-slate-700 -rotate-45 -ml-2'></div>
-                            </div>
+                    <div className='flex justify-center' onClick={() => setShow(!show)}>
+                        <div className={`${show ? 'tranform translate-y-12 ' : 'rotate-180 '} flex justify-center transition duration-1000 cursor-pointer`}>
+                            <div className='h-1 w-4 rounded-md bg-slate-600 rotate-45'></div>
+                            <div className='h-1 w-4 rounded-md bg-slate-600 -rotate-45 -ml-2'></div>
                         </div>
+                    </div>
+                    <div className=' rounded-full bg-indigo-900 bg-opacity-30 '>
                         <div className='flex items-center space-x-5 p-2'>
                             <a href='#header' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'bg-black bg-opacity-20 rounded-full p-1' : ''}>
                                 <img src={home} alt='' className='mix-blend-color-burn' />
