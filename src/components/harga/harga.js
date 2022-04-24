463bd3c2acadb620c0c3a830037c955d03486882
mizabun_ <mizabun010802@gmail.com>
2022-04-24 12:41:50 +0700

    add harga & layanan

--- src/components/harga/harga.js
+++ src/components/harga/harga.js
@@ -2,6 +2,42 @@ import React from 'react'
 
 export default function Harga() {
     return (
-        <h2 className='h-screen'>Harga</h2>
+        <>
+            <h2 className='font-bold text-3xl text-center'>Harga & Layanan</h2>
+            <div className='flex justify-center'>
+                <div className='border border-slate-100 rounded-3xl shadow-lg bg-violet-50 mt-16 py-10 h-fit w-72 text-center space-y-6 '>
+                    <h3 className='text-2xl font-extrabold text-indigo-900'>Standar</h3>
+                    <p className='text-5xl font-bold text-indigo-900'>Gratis</p>
+                    <div className=''>
+                        <p className='text-gray-400 font-normal'>Aplikasi kasir</p>
+                        <p className='text-gray-400 font-normal'>Laporan harian</p>
+                        <p className='text-gray-400 font-normal'>1 user + 1 admin</p>
+                    </div>
+                    <button className='py-2 px-4 bg-indigo-900 rounded-full text-white'>Daftar Sekarang</button>
+                </div>
+
+                <div className='border border-slate-100 rounded-3xl shadow-lg bg-indigo-900 mt-16 py-10 h-fit w-72 text-center space-y-6 mx-6'>
+                    <h3 className='text-2xl font-extrabold text-white'>Standar</h3>
+                    <p className='text-5xl font-bold text-white'>50<span className='text-3xl'>k<sub className='text-sm'>/Bulan</sub></span></p>
+                    <div className=''>
+                        <p className='text-gray-200 font-normal'>Aplikasi kasir</p>
+                        <p className='text-gray-200 font-normal'>Laporan harian</p>
+                        <p className='text-gray-200 font-normal'>3 user + 1 admin</p>
+                    </div>
+                    <button className='py-2 px-4 bg-violet-50 rounded-full text-indigo-900'>Daftar Sekarang</button>
+                </div>
+
+                <div className='border border-slate-100 rounded-3xl shadow-lg bg-violet-50 mt-16 py-10 h-fit w-72 text-center space-y-6 '>
+                    <h3 className='text-2xl font-extrabold text-indigo-900'>Standar</h3>
+                    <p className='text-5xl font-bold text-indigo-900'>75<span className='text-3xl'>k<sub className='text-sm'>/Bulan</sub></span></p>
+                    <div className=''>
+                        <p className='text-gray-400 font-normal'>Aplikasi kasir</p>
+                        <p className='text-gray-400 font-normal'>Laporan harian</p>
+                        <p className='text-gray-400 font-normal'>unlimited user + 1 admin</p>
+                    </div>
+                    <button className='py-2 px-4 bg-indigo-900 rounded-full text-white'>Daftar Sekarang</button>
+                </div>
+            </div>
+        </>
     )
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,7 @@ import Header from '../../components/header/header'
 import Layanan from '../../components/Layanan/layanan'
 import SubHeader from '../../components/subHeader/subHeader'
 import Ellipse1 from '../../asset/Ellipse1.png'
+import Ellipse2 from '../../asset/Ellipse2.png'
 import Rectangle1 from '../../asset/Rectangle1.png'
 import Dot from '../../asset/Dot.png'
 import Rectangle2 from '../../asset/Rectangle2.png'
@@ -26,15 +27,19 @@ export default function Home() {
             </section>
 
             <section id='layanan'>
-                <img src={Rectangle2} alt='' className='absolute translate-y-1/4 right-0 -z-40' />
-                <img src={Rectangle3} alt='' className='absolute mt-64 ml-16 -z-40' />
-                <div className='px-36'>
+                <div className='relative px-36 pb-6'>
+                    <img src={Rectangle2} alt='' className='absolute translate-y-1/4 right-0 -z-40' />
+                    <img src={Rectangle3} alt='' className='absolute mt-64 ml-16 -z-40' />
                     <Layanan />
                 </div>
             </section>
 
             <section id='harga'>
-                <Harga />
+                <div className='relative pt-20 min-h-screen'>
+                    <img src={Dot} alt='' className='absolute -mt-72 left-48 -z-40' />
+                    <img src={Ellipse2} alt='' className='absolute -mt-28 -left-3 -z-40' />
+                    <Harga />
+                </div>
             </section>
 
             <section id='footer'>
